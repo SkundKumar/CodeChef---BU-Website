a1cf826b950db09f448125f4669cd4e0100ed34e
Skund Kumar <brando.skund@gmail.com>
2024-09-06 10:05:40 +0530

    removed particle

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
-import ParticleBackground from "./components/particleBackground";
+
 
 export default function App() {
   return (
     <BrowserRouter>
       
       <div className="relative z-0 bg-hero-pattern ">
-      <ParticleBackground className="z-10 w-screen h-screen bg-red-700" />
-        
+              
           <Navbar />
           <Hero />
         
