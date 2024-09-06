6f961c9f8f9d207a16ae1584f40bedebb450e210
Skund Kumar <brando.skund@gmail.com>
2024-09-06 11:57:34 +0530

    test svg

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -5,9 +5,10 @@ import * as THREE from 'three'
 import { Group } from 'three/examples/jsm/libs/tween.module.js'
 import Loader from './Loader';
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import test from './src/assets/test.svg';
 
 const Scene = () => {
-    
+    let tex = useTexture('./src/assets/test.svg')
     let cyl = useRef(null)
     useFrame((state, delta) => {
         cyl.current.rotation.y += 0.002
@@ -16,7 +17,7 @@ const Scene = () => {
     <group rotation={[0,-0.5,0.2]}>
     <mesh ref={cyl} >
           <cylinderGeometry args={[2.9, 2.9,2.9, 60, 60, true]}/>
-          <meshBasicMaterial  transparent  side={THREE.DoubleSide}/>
+          <meshBasicMaterial map={tex} transparent  side={THREE.DoubleSide}/>
         </mesh>
         </group>
   )
