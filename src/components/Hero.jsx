07ef5599a6532ed662181d07a4bc7826c73f7163
Skund Kumar <brando.skund@gmail.com>
2024-09-17 13:31:19 +0530

    mobil responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,27 +21,36 @@ const Hero = () => {
 
   return (
     <section className='relative w-full h-screen mx-auto'>
-      <div className={"sm:px-16 px-6 absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5"}>
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-sm bg-[#e9dfff]' />
-          <div className='w-1 sm:h-80 h-40 white-gradient ml-2' />
-        </div>
-        <div className='mt-4'>
-          <h1 className={`${styles.heroHeadText} `}>We are </h1>
-          <h1 className={`${styles.heroHeadText} text-[#d6c2fd]`} >CodeChef | BU</h1>
+      <div className="flex flex-col justify-center items-center h-full">
+        {/* Text Container */}
+        <div className={"sm:px-16 px-6 max-w-7xl mx-auto flex flex-row items-start gap-5"}>
+          <div className='flex flex-col justify-center items-center mt-5'>
+            <div className='w-5 h-5 rounded-sm bg-[#e9dfff]' />
+            <div className='w-1 sm:h-80 h-40 white-gradient ml-2' />
+          </div>
+          <div className='mt-4'>
+            <h1 className={`${styles.heroHeadText} `}>We are </h1>
+            <h1 className={`${styles.heroHeadText} text-[#d6c2fd]`} >CodeChef | BU</h1>
 
-          <p className={`mt-3 ml-3 font-sans text-[rgb(206,205,206)]`}>
-            CodeChef’s university-based community chapter promotes
-          </p>
-          <p className={`ml-3 font-sans text-[rgb(206,205,206)]`}>
-            the art of programming among the student community.
-          </p>
+            <p className={`mt-3 ml-3 font-sans text-[rgb(206,205,206)]`}>
+              CodeChef’s university-based community chapter promotes
+            </p>
+            <p className={`ml-3 font-sans text-[rgb(206,205,206)]`}>
+              the art of programming among the student community.
+            </p>
+          </div>
+        </div>
+
+        {/* Image or Canvas */}
+        <div className="w-full h-full mb-56">
+          {isMobile
+            ? <img src={ishanvi2} alt="hero" className='w-full h-full object-contain ' />
+            : <ComputersCanvas />
+          }
         </div>
       </div>
-      {isMobile
-        ? <img src={ishanvi2} alt="hero" className='w-full h-full object-contain mt-20' />
-        : <ComputersCanvas />
-      }
+
+      {/* Scroll Indicator */}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
