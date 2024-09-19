8cbdba46f0e088136a2c909972d9b3a726fea027
Skund Kumar <brando.skund@gmail.com>
2024-09-19 16:00:52 +0530

    paralax

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
 import { About, Experience, Hero, Navbar, Works, StarsCanvas } from './components';
 
+
 export default function App() {
   return (
     <BrowserRouter>
+    
       <ParallaxProvider>
         <div className="relative z-0">
           <Parallax speed={-14}>
+            
             <div className="hero-section">
               <Navbar />
               <Hero />
@@ -29,6 +32,7 @@ export default function App() {
           <StarsCanvas />
         </div>
       </ParallaxProvider>
+      
     </BrowserRouter>
   );
 }
